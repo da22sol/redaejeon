f272da75c8e1b58b7b2e49a1dbaf798c83d8908b
[SW5] 김다솔 <gyeoul299@gmail.com>
2024-03-22 04:47:24 +0900

    style.스타일수정

--- js/js.js
+++ js/js.js
@@ -1,100 +1,110 @@
-   // 메뉴 이벤트
-      // function addon() {
-      //   var onbtns = document.querySelectorAll(
-      //     '#AllMenu .SubMenu > [class^="sub"] li a'
-      //   );
-      //   onbtns.forEach(function (onbtn) {
-      //     onbtn.addEventListener('mouseover', function (event) {
-      //       event.preventDefault();
-      //       onbtn.classList.add('on');
-      //     });
+//  메뉴 이벤트
 
-      //     onbtn.addEventListener('mouseout', function (event) {
-      //       onbtn.classList.remove('on');
-      //     });
-      //   });
-      // }
+function menubtn() {
+  const openbtn = document.querySelector('.SiteMapBtn');
+  const closebtn = document.querySelector('.CloseBtn');
+  const sietwrap = document.querySelector('.SubWrap');
+  const all = document.querySelector('#header');
+  const htmlElement = document.documentElement;
 
-      // window.onload = function () {
-      //   addon();
-      // };
+  const lastMenuItem = document.querySelector(
+    '#AllMenu>li .sub>ul>li:last-child'
+  );
 
-      function menubtn() {
-        const openbtn = document.querySelector('.SiteMapBtn');
-        const closebtn = document.querySelector('.CloseBtn');
-        const sietwrap = document.querySelector('.SubWrap');
-        const all = document.querySelector('#header');
-        const htmlElement = document.documentElement;
+  openbtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    openbtn.style.display = 'none';
+    closebtn.style.display = 'block';
+    sietwrap.style.display = 'block';
+    all.classList.add('all');
+    htmlElement.classList.add('all');
+    lastMenuItem.scrollIntoView({ behavior: 'smooth', block: 'start' });
+  });
 
-        const lastMenuItem = document.querySelector(
-          '#AllMenu>li .sub>ul>li:last-child'
-        );
-
-        openbtn.addEventListener('click', (event) => {
-          event.preventDefault();
-          openbtn.style.display = 'none';
-          closebtn.style.display = 'block';
-          sietwrap.style.display = 'block';
-          all.classList.add('all');
-          htmlElement.classList.add('all');
-          lastMenuItem.scrollIntoView({ behavior: 'smooth', block: 'start' });
-        });
-
-        closebtn.addEventListener('click', (event) => {
-          event.preventDefault();
-          closebtn.style.display = 'none';
-          openbtn.style.display = 'block';
-          sietwrap.style.display = 'none';
-          document.documentElement.classList.remove('all');
-          all.classList.remove('all');
-          htmlElement.classList.remove('all');
-          allMenuBtn.focus();
-        });
-      }
-
-      window.onload = function () {
-        menubtn();
-      };
+  closebtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    closebtn.style.display = 'none';
+    openbtn.style.display = 'block';
+    sietwrap.style.display = 'none';
+    document.documentElement.classList.remove('all');
+    all.classList.remove('all');
+    htmlElement.classList.remove('all');
+    allMenuBtn.focus();
+  });
+}
 
 $(document).ready(function () {
-  
+  function gnb() {
+    var parent = $('#gnb');
+    var dep1 = parent.find('>li>a');
+    var sub = parent.find('>li>.sub');
+    var bg = $('.subM_Bg');
+    var last = parent.find('>li:last .sub ul li:last a');
+    bg.css({ height: 0, display: 'none' });
 
-    function gnb() {
-        var parent = $("#gnb");
-        var dep1 = parent.find(">li>a");
-        var sub = parent.find(">li>.sub");
-        var bg = $(".subM_Bg");
-        var last = parent.find(">li:last .sub ul li:last a");
-        bg.css({ height: 0, display: "none" });
+    dep1.on('mouseover focusin', function () {
+      var t = $(this);
+      var menuH = t.next().outerHeight();
 
-        dep1.on("mouseover focusin", function () {
-            var t = $(this);
-            var menuH = t.next().outerHeight();
+      if (t.next().length) {
+        dep1.removeClass('on');
+        sub.stop().hide();
+        t.addClass('on');
+        t.next().stop().show();
+        bg.show().stop().css({ height: menuH });
+      } else {
+        gnbHide();
+      }
+    });
 
-            if (t.next().length) {
-                dep1.removeClass("on");
-                sub.stop().hide();
-                t.addClass("on");
-                t.next().stop().show();
-                bg.show().stop().css({ height: menuH });
-            } else {
-                gnbHide();
-            }
-        });
+    last.on('focusout', function () {
+      gnbHide();
+    });
 
-        last.on("focusout", function () {
-            gnbHide();
-        });
+    parent.on('mouseleave', function () {
+      gnbHide();
+    });
 
-        parent.on("mouseleave", function () {
-            gnbHide();
-        });
-
-        function gnbHide() {
-            dep1.removeClass("on");
-            sub.stop().hide();
-            bg.hide();
-        }
+    function gnbHide() {
+      dep1.removeClass('on');
+      sub.stop().hide();
+      bg.hide();
     }
-    gnb();
+  }
+  gnb();
 });
+document.addEventListener('DOMContentLoaded', function() {
+  const onbtns = document.querySelectorAll('.NoticeBtn');
+  const descs = document.querySelectorAll('.NoticeDesc');
+
+  // 첫 번째 버튼과 해당하는 Description에 'on' 클래스 추가
+  onbtns[0].classList.add('on');
+  descs[0].style.display = 'block';
+
+  onbtns.forEach(function(onbtn) {
+      onbtn.addEventListener('click', function() {
+          const tabNumber = this.getAttribute('data-tab-number');
+
+          // 모든 버튼에서 'on' 클래스 제거
+          onbtns.forEach(function(btn) {
+              btn.classList.remove('on');
+          });
+
+          // 모든 Description 영역 숨기기
+          descs.forEach(function(desc) {
+              desc.style.display = 'none';
+          });
+
+          // 선택된 버튼에 'on' 클래스 추가
+          this.classList.add('on');
+
+          // 해당하는 Description 영역 보이기
+          const selectedDesc = document.querySelector('.NoticeDesc[data-tab-number="' + tabNumber + '"]');
+          selectedDesc.style.display = 'block';
+      });
+  });
+});
+window.onload = function () {
+  noticeon();
+  menubtn();
+};
